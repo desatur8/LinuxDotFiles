93f3df396c0eed62332464918d974cbc8cca64b1
Shaun Muller <muller.shaun@gmail.com>
2024-10-11 09:48:19 +0200

    chatGPT, changed tray to far right

--- .config/ml4w/settings/ai.sh
+++ .config/ml4w/settings/ai.sh
@@ -1,1 +1,2 @@
-$(cat ~/.config/ml4w/settings/browser.sh) --new-window https://chat.openai.com
+#$(cat ~/.config/ml4w/settings/browser.sh) --new-window https://chat.openai.com
+gtk-launch WebApp-ChatGPT0620.desktop
